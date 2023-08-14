177fe04ee228d020f5a383ace581ca72f46eddad
echonorthernlights <elhassanianas@gmail.com>
2023-08-14 18:06:26 +0100

    models (User, Message,Chat)

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    //   image : {
+    //     type :String,
+    //     required : false,
+    //     default : "/images/users/avatar.png"
+    //   },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
