3701c16395072c00757adc9b277262f7173db04f
echonorthernlights <elhassanianas@gmail.com>
2023-08-14 20:19:53 +0100

    added (login + logout) routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,19 @@
 import express from "express";
 import {
+  auth,
   getUserById,
   getUsers,
+  logoutUser,
   register,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(getUsers);
 router.route("/register").post(register);
-router.route("/:id").get(getUserById);
+router.route("/login").post(auth);
+router.route("/logout").post(logoutUser);
+router.route("/:id").get(protect, getUserById);
 
 export default router;
